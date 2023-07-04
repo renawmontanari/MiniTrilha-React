28e4907486ca0bceee77c3ab4cc22b380fad5cdc
renawmontanari <regiangelica@gmail.com>
2023-07-04 20:15:18 -0300

    MiniTrilha-React

---
+++ hello-world/src/components/ManageData.js
@@ -0,0 +1,14 @@
+
+const ManageData = () => {
+    const someData = 10;
+
+    return (
+        <div>
+            <div>
+                <p>Valor: {someData}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ManageData
