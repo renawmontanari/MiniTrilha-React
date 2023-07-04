017698fee6b1e8d613b1ba3cc495b071123b03ba
renawmontanari <regiangelica@gmail.com>
2023-07-04 20:39:38 -0300

    MiniTrilha-React

---
+++ useState/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ManageData from './components/ManageData';
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Hello-World</h1>
+        <ManageData />
+      </div>
+    </>
+  );
+}
+
+export default App 
