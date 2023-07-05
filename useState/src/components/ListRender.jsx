2ec583d59a0b5591b8ad1bbf256adf7c3fa98c45
renawmontanari <regiangelica@gmail.com>
2023-07-05 20:07:10 -0300

    MiniTrilha-React

--- useState/src/components/ListRender.jsx
+++ useState/src/components/ListRender.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react"
 
 const ListRender = () => {
-    const [list] = useState(["Renan", "João", "Josias", "Carmen"]);
+  const [list] = useState(["Renan", "João", "Josias", "Carmen"]);
+  
+  const [users] = useState([
+    { id: 1, name: "Pedro", age: 32 },
+    { id: 345, name: "Marcus", age: 45 },
+    { id: 3467, name: "Maria", age: 25 },
+  ]);
 
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
+            ))}
+        </ul>
+        <ul>
+            {users.map((itemUsers) => (
+                <li key={itemUsers.id}>Nome: {itemUsers.name} - 
+                Idade: {itemUsers.age}</li>
             ))}
         </ul>
     </div>
