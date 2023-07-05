028ebc3c6a1d283538d0aa3a4adbe87cf97c2cdc
renawmontanari <regiangelica@gmail.com>
2023-07-05 20:35:08 -0300

    MiniTrilha-React

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
 
@@ -11,6 +12,7 @@ function App() {
         <h1>Hello-World</h1>
         <ManageData />
         <ListRender />
+        <ConditionalRender />
       </div>
     </>
   );
