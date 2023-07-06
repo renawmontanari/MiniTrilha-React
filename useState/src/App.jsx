ff04e42a229b668a7422a627cfd0e3331c6fc90f
renawmontanari <regiangelica@gmail.com>
2023-07-06 20:24:58 -0300

    MiniTrilha-React

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,10 +1,16 @@
 
+import { useState } from 'react';
 import './App.css'
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+
+  const name = "Renan";
+
+  const [userName] = useState("Maria");
 
   return (
     <>
@@ -13,6 +19,7 @@ function App() {
         <ManageData />
         <ListRender />
         <ConditionalRender />
+        <ShowUserName name={userName} />
       </div>
     </>
   );
